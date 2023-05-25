2fbdf906892979d311999db8eda940437fd8d7eb
Aiden Vu <100857445+AidenVu1@users.noreply.github.com>
2023-05-25 14:14:21 +0000

    change to post

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
                   </div>
                 </header>
               <main class="mdl-layout__content" style="margin-left:75px;">
-              <form action="" method="GET" class="mdl-grid" autocomplete="off">
+              <form action="" method="POST" class="mdl-grid" autocomplete="off">
     <div class="mdl-cell mdl-cell--12-col">
         <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
             <input class="mdl-textfield__input" type="text" id="iterations" name="iterations" pattern="[0-9]+" title="Invalid input">
@@ -44,18 +44,19 @@ function calculate_pi($iterations) {
     return $pi;
 }
 
-if(isset($_GET['iterations']) && $_GET['iterations'] >= 0){
-    $iterations = intval($_GET['iterations']);
+if(isset($_POST['iterations']) && $_POST['iterations'] >= 0){
+    $iterations = intval($_POST['iterations']);
     $pi_value = calculate_pi($iterations);
     echo "<div class='results'>";
     echo "Iterations: $iterations <br>";
     echo "Estimated value of Pi: $pi_value <br>";
     echo "</div>";
 }
-elseif(isset($_GET['iterations']) && $_GET['iterations'] < 0){
+elseif(isset($_POST['iterations']) && $_POST['iterations'] < 0){
     echo "<div class='results'>Please enter a non-negative integer only.</div>";
 }
 ?>
+
 
 
       </main>
